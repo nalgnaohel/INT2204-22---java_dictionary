58d940b1c9c4b3179a1de7303747e165a68a9707
Khuat Tuan Anh <ppmelon2004@gmail.com>
2023-11-02 23:15:58 +0700

    button click added

--- src/main/java/com/example/java_dictionary/Controller.java
+++ src/main/java/com/example/java_dictionary/Controller.java
@@ -1,14 +1,117 @@
 package com.example.java_dictionary;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.fxml.Initializable;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controller {
-    @FXML
-    private Label welcomeText;
+public class Controller implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button lookupButton;
+    @FXML
+    private Button translateButton;
+    @FXML
+    private Button gameButton;
+    @FXML
+    private Button importButton;
+    @FXML
+    private Button exportButton;
+    private Button currentButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
-}
+
+    @FXML
+    protected void onMouseEntered(MouseEvent event) {
+        // Xử lý sự kiện khi di chuột vào đối tượng
+        Button b = (Button) event.getSource();
+        if (!b.equals(currentButton)) {
+            b.getParent().getStyleClass().clear();
+            b.getParent().getStyleClass().add("tab-dragged");
+        }
+    }
+
+    @FXML
+    protected void onMouseExited(MouseEvent event) {
+        Button b = (Button) event.getSource();
+        if (!b.equals(currentButton)) {
+            b.getParent().getStyleClass().clear();
+            b.getParent().getStyleClass().add("tab-menu");
+        }
+    }
+
+    @FXML
+    protected void ButtonClick(ActionEvent event) {
+        if (currentButton != null) {
+            currentButton.getParent().getStyleClass().clear();
+            currentButton.getParent().getStyleClass().add("tab-menu");
+        }
+
+        currentButton = (Button) event.getSource();
+        currentButton.getParent().getStyleClass().clear();
+        currentButton.getParent().getStyleClass().add("tab-selected");
+    }
+
+
+
+
+    // error ?? idk
+//    @FXML
+//    protected void ButtonDragIn(ActionEvent event) {
+//        Button b = (Button) event.getSource();
+//        b.getParent().getStyleClass().clear();
+//        b.getParent().getStyleClass().add("tab-dragged");
+//    }
+//
+//    @FXML
+//    protected void ButtonDragOut(ActionEvent event) {
+//        Button b = (Button) event.getSource();
+//        b.getParent().getStyleClass().clear();
+//        b.getParent().getStyleClass().remove("tab-menu");
+    }
+
+//    @FXML
+//    private Button translateTab;
+//
+//    @FXML
+//    private Button gameTab;
+//
+//    @FXML
+//    private Button importTab;
+//
+//    @FXML
+//    private Button exportTab;
+
+//    @FXML
+//    private AnchorPane ClientArea;
+//
+//    private AnchorPane LookUpTabArea;
+
+//    private LookUpTabController lookupTabController;
+
+//    public void initialize(URL url, ResourceBundle resourceBundle) {
+//        try {
+//            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("LookUpTab.fxml"));
+//            LookUpTabArea = fxmlLoader.load();
+//            lookupTabController = fxmlLoader.getController();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    private void handleAction(ActionEvent event) {
+//        if (event.getSource() == lookupTab) {
+//            this.ClientArea.getChildren().setAll(LookUpTabArea);
+//        }
+//    }
---
+++ src/main/java/com/example/java_dictionary/LookUpTabController.java
@@ -0,0 +1,4 @@
+package com.example.java_dictionary;
+
+public class LookUpTabController {
+}
