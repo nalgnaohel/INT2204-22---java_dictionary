8e10fbd6a1ef842ffedebb42d18ee9772f50dbe1
Khuat Tuan Anh <ppmelon2004@gmail.com>
2023-11-03 02:57:10 +0700

    button click update (tab-dragged color + color adjust effect)

--- src/main/java/ui/controller/Controller.java
+++ src/main/java/ui/controller/Controller.java
@@ -3,12 +3,17 @@ package ui.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
-public class Controller {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
 
     @FXML
     private Button lookupButton;
@@ -20,7 +25,12 @@ public class Controller {
     private Button importButton;
     @FXML
     private Button exportButton;
+    @FXML
+    private AnchorPane contentArea;
+
     private Button currentButton;
+    private AnchorPane lookupArea;
+    private LookUpTabController lookupTabController;
 
     @FXML
     protected void onMouseEntered(MouseEvent event) {
@@ -54,7 +64,7 @@ public class Controller {
     }
 
     @FXML
-    protected void ButtonClick(ActionEvent event) {
+    protected void ButtonClick(ActionEvent event) throws IOException {
         if (currentButton != null) {
             currentButton.getParent().getStyleClass().clear();
             currentButton.getParent().getChildrenUnmodifiable().get(0).setEffect(null);
@@ -67,57 +77,18 @@ public class Controller {
         currentButton.getParent().getChildrenUnmodifiable().get(0).setEffect(null);
         currentButton.getParent().getChildrenUnmodifiable().get(1).setEffect(null);
         currentButton.getParent().getStyleClass().add("tab-selected");
+
+        contentArea.getChildren().setAll(lookupArea);
     }
 
-
-
-
-    // error ?? idk
-//    @FXML
-//    protected void ButtonDragIn(ActionEvent event) {
-//        Button b = (Button) event.getSource();
-//        b.getParent().getStyleClass().clear();
-//        b.getParent().getStyleClass().add("tab-dragged");
-//    }
-//
-//    @FXML
-//    protected void ButtonDragOut(ActionEvent event) {
-//        Button b = (Button) event.getSource();
-//        b.getParent().getStyleClass().clear();
-//        b.getParent().getStyleClass().remove("tab-menu");
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ui/fxml/lookupTab.fxml"));
+            lookupArea = fxmlLoader.load();
+            lookupTabController = fxmlLoader.getController();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-
-//    @FXML
-//    private Button translateTab;
-//
-//    @FXML
-//    private Button gameTab;
-//
-//    @FXML
-//    private Button importTab;
-//
-//    @FXML
-//    private Button exportTab;
-
-//    @FXML
-//    private AnchorPane ClientArea;
-//
-//    private AnchorPane LookUpTabArea;
-
-//    private LookUpTabController lookupTabController;
-
-//    public void initialize(URL url, ResourceBundle resourceBundle) {
-//        try {
-//            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("LookUpTab.fxml"));
-//            LookUpTabArea = fxmlLoader.load();
-//            lookupTabController = fxmlLoader.getController();
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    private void handleAction(ActionEvent event) {
-//        if (event.getSource() == lookupTab) {
-//            this.ClientArea.getChildren().setAll(LookUpTabArea);
-//        }
-//    }
+}
--- src/main/java/ui/controller/LookUpTabController.java
+++ src/main/java/ui/controller/LookUpTabController.java
@@ -1,4 +1,11 @@
 package ui.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class LookUpTabController {
+    @FXML
+    private TextField searchbox;
 }
