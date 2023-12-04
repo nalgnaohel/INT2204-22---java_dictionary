1a7a3bc4f35d8011000ae5cd6b000b2dc3f32123
Khuat Tuan Anh <ppmelon2004@gmail.com>
2023-12-04 18:50:52 +0700

    UI Update (in general :v) pt2 (fix lỗi css)

--- src/main/java/dictionary/ui/game/finding/QuestionWindow.java
+++ src/main/java/dictionary/ui/game/finding/QuestionWindow.java
@@ -4,6 +4,7 @@ import dictionary.Main;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -17,7 +18,7 @@ public class QuestionWindow {
     private FindingController findingController;
     public void display(final ArrayList<String> question) throws IOException {
         stage = new Stage();
-        stage.initStyle(StageStyle.UNDECORATED);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.initOwner(findingController.getFindingMainWindow().getGameStage());
         stage.initModality(Modality.WINDOW_MODAL);
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/question.fxml"));
@@ -27,11 +28,12 @@ public class QuestionWindow {
         questionController.init(question);
 
         Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
         stage.setScene(scene);
-        stage.setX(stage.getOwner().getX());
-        stage.setY(stage.getOwner().getY() + stage.getOwner().getHeight() - 450);
-        stage.setHeight(450);
-        stage.setWidth(800);
+//        stage.setX(stage.getOwner().getX());
+//        stage.setY(stage.getOwner().getY() + stage.getOwner().getHeight() - 450);
+//        stage.setHeight(450);
+//        stage.setWidth(800);
         stage.setResizable(false);
         stage.showAndWait();
         stage.setOnCloseRequest(windowEvent -> {
